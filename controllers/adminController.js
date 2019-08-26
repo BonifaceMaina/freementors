9521a68b26e69bd09b00b64cbc4f94182327b1b7
Boniface <karis.bm@gmail.com>
2019-08-26 19:36:04 +0300

    feature: add functionality for admin to upgrade users

---
+++ controllers/adminController.js
@@ -0,0 +1,28 @@
+const users = require('../models/usersModel');
+
+class AdminController{
+    static upgradeUser(req, res){
+        // const mentorSession = sessions.find(sessions => sessions.sessionId == req.params.sessionId);
+        const userToUpgrade = users.find(user => user.id == req.params.userId);
+        if(req.user.admin == true){
+            if(userToUpgrade){
+                if(userToUpgrade.isMentor == false){
+                    userToUpgrade.isMentor = true;
+                    res.status(200).json({
+                        status: 200,
+                        message: 'User account changed to mentor',
+                        data: userToUpgrade
+                    });
+                }else{
+                    return res.status(400).json({ status: 400, message: 'User is already a mentor'});
+                }
+            }else{
+                return res.status(400).json({ status: 404, message: 'User not found'});
+            }
+        }else{
+            return res.status(403).json({ status: 403, message: 'Unauthorized access'});
+        }
+    }
+}
+
+module.exports = AdminController;
--- tests/adminTests.js
+++ tests/adminTests.js
@@ -1,13 +1,14 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 const expect = chai.expect;
-const assert = require('assert');
 
 describe('admin functionalities', function(){
 
     it('should upgrade user', function(done){
         chai.request("http://localhost:3000/api/v1")
         .patch('/user/3')
-        .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZmlyc3ROYW1lIjoiQm9uaWZhY2UiLCJlbWFpbCI6ImthcmlzYm1AZ21haWwuY29tIiwiaXNNZW50b3IiOmZhbHNlLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTY2ODMxMTA2fQ.adZcrILRdsWXdS2q9daIob4Hbgvhcgj9jeLZbq1WxtY')
+        .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZmlyc3ROYW1lIjoiQm9uaWZhY2UiLCJlbWFpbCI6ImthcmlzYm1AZ21haWwuY29tIiwiaXNNZW50b3IiOmZhbHNlLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTY2ODMzMjQzfQ.HdpITe3pB6nmmEtragbocaVP1DM9fa5uy_awgdcx0I4')
         .end((error,response) => {
             expect(response.statusCode).to.equal(200);
             expect(response).to.be.an('object');
@@ -30,17 +31,17 @@ describe('admin functionalities', function(){
 
     it('should not upgrade non-existent user', function(done){
         chai.request("http://localhost:3000/api/v1")
-        .patch('/user/476')
-        .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZmlyc3ROYW1lIjoiQm9uaWZhY2UiLCJlbWFpbCI6ImthcmlzYm1AZ21haWwuY29tIiwiaXNNZW50b3IiOmZhbHNlLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTY2ODMxMTA2fQ.adZcrILRdsWXdS2q9daIob4Hbgvhcgj9jeLZbq1WxtY')
+        .patch('/user/47')
+        .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZmlyc3ROYW1lIjoiU2FtIiwiZW1haWwiOiJzYW1AZ21haWwuY29tIiwiaXNNZW50b3IiOmZhbHNlLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU2NjgzMTMzM30.203U8j9a7TtBNZYj0bR6qEIct0Bk2EG6BizhxVj-Ivc')
         .end((error,response) => {
-            expect(response.statusCode).to.equal(404);
+            expect(response.statusCode).to.equal(403);
             expect(response).to.be.an('object');
             if(error) done(error);
             done();        
         });
     });
 
-    it('should not upgrade non-mentor users', function(done){
+    it('should not upgrade mentor users', function(done){
         chai.request("http://localhost:3000/api/v1")
         .patch('/user/1')
         .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZmlyc3ROYW1lIjoiQm9uaWZhY2UiLCJlbWFpbCI6ImthcmlzYm1AZ21haWwuY29tIiwiaXNNZW50b3IiOmZhbHNlLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTY2ODMxMTA2fQ.adZcrILRdsWXdS2q9daIob4Hbgvhcgj9jeLZbq1WxtY')
