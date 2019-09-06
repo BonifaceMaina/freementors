59bcbd8f8ce07564e3806b5c3dcab1d32b398a6f
Boniface <karis.bm@gmail.com>
2019-09-06 12:45:27 +0300

    chore(tests): move user data into a mocks file [Finishes #168339973]

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,11 +7,6 @@ class AdminController {
     mentors.push(req.user);
     const data = req.user;
     responseHelper.successMessage(200, 'User account changed to mentor', data, res);
-    // res.status(200).json({
-    //   status: 200,
-    //   message: 'User account changed to mentor',
-    //   data: req.user,
-    // });
   }
 }
 
